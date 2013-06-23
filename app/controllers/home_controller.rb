cf68d34936b343ac54388342ac0d916cf2a7abf9
Arnaud Besnier <arnaudibesnier@gmail.com>
2013-06-23 23:37:56 +0200

    Improve the responsiveness of the home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,12 +2,12 @@ class HomeController < ApplicationController
 
   def index
   	@social_networks = [
-	  { name: 'facebook',    edito: 'without friends you are nothing', alt: 'Facebook',  url: 'http://www.facebook.com/arnaud.besnier' },
-	  { name: 'twitter',     edito: 'say something', alt: 'Twitter',   url: 'http://twitter.com/arnaud_besnier' },
+	  { name: 'facebook',    edito: "don't forget real friends",  alt: 'Facebook',  url: 'http://www.facebook.com/arnaud.besnier' },
+	  { name: 'twitter',     edito: 'say something',              alt: 'Twitter',   url: 'http://twitter.com/arnaud_besnier' },
 	  { name: 'google_plus', edito: 'circle some great articles', alt: 'Google+',   url: 'https://plus.google.com/u/1/108371618713321616542/posts' },
-	  { name: 'instagram',   edito: 'capture Life Moments', alt: 'Instagram', url: 'http://instagram.com/ribens' },
-	  { name: 'linkedin',    edito: 'skills for head hunters', alt: 'LinkedIn',  url: 'http://fr.linkedin.com/in/arnaudbesnier' },
-	  { name: 'github',      edito: 'hackers playground', alt: 'Github',    url: 'https://github.com/arnaudbesnier' }
+	  { name: 'instagram',   edito: 'capture life moments',       alt: 'Instagram', url: 'http://instagram.com/ribens' },
+	  { name: 'linkedin',    edito: 'skills for head hunters',    alt: 'LinkedIn',  url: 'http://fr.linkedin.com/in/arnaudbesnier' },
+	  { name: 'github',      edito: 'hackers playground',         alt: 'Github',    url: 'https://github.com/arnaudbesnier' }
 	]
   end
 
