7d1684c99cad9278b577e7c2f0778f990afcf830
Arnaud Besnier <arnaudibesnier@gmail.com>
2013-07-07 17:25:05 +0200

    Improve the main layout

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,14 +1,2 @@
 class HomeController < ApplicationController
-
-  def index
-  	@social_networks = [
-	  { name: 'facebook',    edito: "don't forget real friends",  alt: 'Facebook',  url: 'http://www.facebook.com/arnaud.besnier' },
-	  { name: 'twitter',     edito: 'say something',              alt: 'Twitter',   url: 'http://twitter.com/arnaud_besnier' },
-	  { name: 'google_plus', edito: 'circle some great articles', alt: 'Google+',   url: 'https://plus.google.com/u/1/108371618713321616542/posts' },
-	  { name: 'instagram',   edito: 'capture life moments',       alt: 'Instagram', url: 'http://instagram.com/ribens' },
-	  { name: 'linkedin',    edito: 'skills for head hunters',    alt: 'LinkedIn',  url: 'http://fr.linkedin.com/in/arnaudbesnier' },
-	  { name: 'github',      edito: 'hackers playground',         alt: 'Github',    url: 'https://github.com/arnaudbesnier' }
-	]
-  end
-
 end
