b81e9b2eaffa1e16757088604d0c6ddae696d549
Arnaud Besnier <arnaudibesnier@gmail.com>
2013-07-07 17:29:58 +0200

    Redirect all routes to the home page

--- config/routes.rb
+++ config/routes.rb
@@ -49,6 +49,7 @@ PersonalWebsite::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   root :to => 'home#index'
+  match '*path' => redirect('/')
 
   # See how all your routes lay out with "rake routes"
 
