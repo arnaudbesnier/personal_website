5573086153cf45879c18347d0dac6b6f5b3516b8
Arnaud Besnier <arnaudIbesnier@gmail.com>
2014-12-15 23:08:59 +0100

    Add my first activity thinking

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,10 @@ PersonalWebsite::Application.routes.draw do
 
   root to: redirect('/photos')
 
-  resources :photos, only: [:index]
-  resources :posts,  only: [:index]
-  resources :resume, only: [:index]
+  resources :photos,   only: [:index]
+  # resources :posts,    only: [:index]
+  resources :activity, only: [:index]
+  resources :resume,   only: [:index]
 
   # WARNING: keep this at the bottom of the file
   match '*path' => redirect('/photos')
